119b712c8576897e36da7a45cf0aef5fc73d12fc
David-Navarro-Oliver <navarropalle@gmail.com>
2026-03-09 22:07:00 +0100

    feat: print FizzBuzz sequence from 1 to 15 in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,6 +16,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Main main = new Main();
 
+        for (int i = 1; i <= 15; i++) {
+            System.out.println(main.convert(i));
+        }
     }
 }
