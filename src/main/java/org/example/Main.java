73f0fb1a18cf1b54fb6786fdc881ebb0388e0aed
David-Navarro-Oliver <navarropalle@gmail.com>
2026-03-09 21:32:24 +0100

    feat: implement first FizzBuzz conversion for input 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Main {
+
+    public String convert(int number) {
+        return "1";
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package fizzbuzz;
 
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void shouldReturn1WhenInputIs1() {
+        Main main = new Main();
+        assertEquals("1", main.convert(1));
+    }
 }
